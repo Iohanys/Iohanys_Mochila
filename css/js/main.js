8df3f19152c7cdb12a9a592eedef9ecde46fa862
Iohanys <134975916+Iohanys@users.noreply.github.com>
2023-11-20 19:58:48 -0300

    RR

---
+++ css/js/main.js
@@ -0,0 +1,30 @@
+const form = console.log(document.getElementById("novoItem"))
+
+form.addEventListener("submit", (evento)=>{
+  evento.prevenDefaut()
+
+
+  console.log()
+  console.log(eventoevento.target.elements['quantidade'].value)
+
+
+  criaElemento(evento.target.elements['nome'].value, quantidade)
+})
+
+function criaElemento(nome,quantidade) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
